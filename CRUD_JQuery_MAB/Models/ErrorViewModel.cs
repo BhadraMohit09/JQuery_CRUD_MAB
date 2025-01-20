180c13c3a31e8abf82d79941b8d4348b80a9f140
BhadraMohit09 <mohitbhadra27@gmail.com>
2025-01-20 19:30:04 +0530

    Add project files.

---
+++ CRUD_JQuery_MAB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_JQuery_MAB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
